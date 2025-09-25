96314d41a065fdbd51b3786b1279aad258a59180
Njoya samuel <samuelnjoya08@gmail.com>
2025-09-25 06:12:27 +0100

    ajoute de la vue

--- monBlog/blog/views.py
+++ monBlog/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import post
 
-# Create your views here.
+def post_list(request):
+    posts = post.objects.all()
+    # print(posts)
+    return render(request, 'blog/post/list.html' ,{'posts':posts}) #ajouter un dictionnaire facultatif , {} apres le name
+
+def post_detail(request, slug:str):
+    # try:
+    #     post_dt  = post.objects.get(slug=slug)
+    # except post.DoesNotExist:
+    #     raise('this post doesn\'t exist' )
+    post_dt = get_object_or_404(post, slug=slug)
+        
+    return render(request, 'blog/post/detail.html',{'post':post_dt})
--- monBlog/monBlog/settings.py
+++ monBlog/monBlog/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'blog', # nom de notre Application 
 ]
 
 MIDDLEWARE = [
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'monBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
